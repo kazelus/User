7a9b62e98fcc62f63864c2dad10b9ed50a1d138f
kazelus <m.kolaczynski98@gmail.com>
2017-05-12 06:51:40 +0200

    Add files via upload

---
+++ UserRestControler (1).java
@@ -0,0 +1,43 @@
+package pl.com.oands.controler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.com.oands.model.User;
+import pl.com.oands.service.UserService;
+
+@RestController
+public class UserRestControler {
+
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody void addUser(@RequestBody User user) {
+		userService.addUser(user);
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody void editUser(@RequestBody User user, @PathVariable int id ) {
+		userService.editUser(user, id);
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody void deleteUser(@PathVariable int id) {
+		userService.deleteUser(id);
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<User> getUser() {
+		return userService.getUsers();
+	}
+
+}
---
+++ UserService (1).java
@@ -0,0 +1,15 @@
+package pl.com.oands.service;
+
+import java.util.List;
+
+import pl.com.oands.model.User;
+
+public interface UserService {
+	public void addUser(User user);
+
+	public void editUser(User user, int id);
+
+	public void deleteUser(int id);
+
+	public List<User> getUsers();
+}
